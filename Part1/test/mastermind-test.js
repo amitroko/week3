63be8d25091f49cdfe5edb8d48102aed37ca20d6
Alec Mitrokostas <amitroko@yahoo.com>
2022-05-24 21:47:45 -0400

    submission

--- Part1/test/mastermind-test.js
+++ Part1/test/mastermind-test.js
@@ -1,1 +1,117 @@
 //[assignment] write your own unit test to show that your Mastermind variation circuit is working as expected
+const chai = require("chai");
+const path = require("path");
+const { isTypedArray } = require("util/types");
+const wasm_tester = require("circom_tester").wasm;
+const F1Field = require("ffjavascript").F1Field;
+const Scalar = require("ffjavascript").Scalar;
+exports.p = Scalar.fromString("21888242871839275222246405745257275088548364400416034343698204186575808495617");
+const Fr = new F1Field(exports.p);
+const assert = chai.assert;
+const buildPoseidon = require("circomlibjs").buildPoseidon;
+
+describe("Super Mastermind tests", () => {
+    var poseidon;
+    var F;
+
+    before(async() => {
+        poseidon = await buildPoseidon();
+        F = poseidon.F;
+    });
+
+    it("Should work when the codebreaker has no hits", async() => {
+        const circuit = await wasm_tester("contracts/circuits/MastermindVariation.circom");
+        await circuit.loadConstraints();
+
+        let salt = 10;
+        const guesses = [0, 1, 2, 3, 4];
+        const pubSolnHash = poseidon([salt, 5, 6, 7, 0, 1]);
+        const bigIntHash = F.toObject(pubSolnHash);
+
+        const INPUT = {
+            "pubGuessA": guesses[0],
+            "pubGuessB": guesses[1],
+            "pubGuessC": guesses[2],
+            "pubGuessD": guesses[3],
+            "pubGuessE": guesses[4],
+            "pubSolnHash": bigIntHash.toString(),
+            "pubNumHit": 0,
+            "pubNumBlow": 2,
+            "privSolnA": 5,
+            "privSolnB": 6,
+            "privSolnC": 7,
+            "privSolnD": 0,
+            "privSolnE": 1,
+            "privSalt": salt,
+        };
+
+        const witness = await circuit.calculateWitness(INPUT, true);
+
+        assert(Fr.eq(Fr.e(witness[0]), Fr.e(1)));
+        assert(Fr.eq(Fr.e(witness[1]), Fr.e(bigIntHash)));
+    });
+
+    it("Should work when the codebreaker guesses the code", async() => {
+        const circuit = await wasm_tester("contracts/circuits/MastermindVariation.circom");
+        await circuit.loadConstraints();
+
+        let salt = 10;
+        const guesses = [0, 1, 2, 3, 4];
+        const pubSolnHash = poseidon([salt, 0, 1, 2, 3, 4]);
+        const bigIntHash = F.toObject(pubSolnHash);
+
+        const INPUT = {
+            "pubGuessA": guesses[0],
+            "pubGuessB": guesses[1],
+            "pubGuessC": guesses[2],
+            "pubGuessD": guesses[3],
+            "pubGuessE": guesses[4],
+            "pubSolnHash": bigIntHash.toString(),
+            "pubNumHit": 5,
+            "pubNumBlow": 0,
+            "privSolnA": guesses[0],
+            "privSolnB": guesses[1],
+            "privSolnC": guesses[2],
+            "privSolnD": guesses[3],
+            "privSolnE": guesses[4],
+            "privSalt": salt,
+        };
+
+        const witness = await circuit.calculateWitness(INPUT, true);
+
+        assert(Fr.eq(Fr.e(witness[0]), Fr.e(1)));
+        assert(Fr.eq(Fr.e(witness[1]), Fr.e(bigIntHash)));
+    });
+
+    it("Should work when the codebreaker has some hits", async() => {
+        const circuit = await wasm_tester("contracts/circuits/MastermindVariation.circom");
+        await circuit.loadConstraints();
+
+        let salt = 10;
+        const guesses = [0, 1, 2, 3, 4];
+        const pubSolnHash = poseidon([salt, 0, 1, 4, 5, 6]);
+        const bigIntHash = F.toObject(pubSolnHash);
+
+        const INPUT = {
+            "pubGuessA": guesses[0],
+            "pubGuessB": guesses[1],
+            "pubGuessC": guesses[2],
+            "pubGuessD": guesses[3],
+            "pubGuessE": guesses[4],
+            "pubSolnHash": bigIntHash.toString(),
+            "pubNumHit": 2,
+            "pubNumBlow": 1,
+            "privSolnA": 0,
+            "privSolnB": 1,
+            "privSolnC": 4,
+            "privSolnD": 5,
+            "privSolnE": 6,
+            "privSalt": salt,
+        };
+
+        const witness = await circuit.calculateWitness(INPUT, true);
+
+        assert(Fr.eq(Fr.e(witness[0]), Fr.e(1)));
+        assert(Fr.eq(Fr.e(witness[1]), Fr.e(bigIntHash)));
+    });
+}).timeout(100000000);
--- Part2/src/index.ts
+++ Part2/src/index.ts
@@ -239,6 +239,19 @@ const encrypt = async (
 ): Promise<Ciphertext> => {
   const mimc7 = await buildMimc7();
   // [assignment] generate the IV, use Mimc7 to hash the shared key with the IV, then encrypt the plain text
+  const iv = mimc7.multiHash(plaintext, BigInt(0));
+
+  const ciphertext: Ciphertext = {
+    iv,
+    data: plaintext.map((e: bigint, i: number): bigint => {
+      return e + mimc7.F.toObject(mimc7.hash(
+        sharedKey,
+        mimc7.F.toObject(iv) + BigInt(i),
+      ));
+    }),
+  };
+
+  return ciphertext;
 };
 
 /*
@@ -250,6 +263,16 @@ const decrypt = async (
   sharedKey: EcdhSharedKey,
 ): Promise<Plaintext> => {
   // [assignment] use Mimc7 to hash the shared key with the IV, then descrypt the ciphertext
+  const mimc7 = await buildMimc7();
+
+  const plaintext: Plaintext = ciphertext.data.map(
+    (e: bigint, i: number): bigint => {
+      let hash = mimc7.hash(sharedKey, mimc7.F.toObject(ciphertext.iv) + BigInt(i));
+      return BigInt(e) - BigInt(mimc7.F.toObject(hash));
+    }
+  );
+
+  return plaintext;
 };
 
 export {
